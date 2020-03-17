d222029aca43eb6e1396ace769be4ed71a730716
AmandeepBhupal <amandeepbhupal96@gmail.com>
2020-03-16 23:45:12 -0700

    First Commit

--- myscript.sh
+++ myscript.sh
@@ -31,12 +31,24 @@ NAME="AJ"
 
 
 # ELSE-IF (elif)
-if [ "$NAME" == "AD" ]
-then 
-    echo "Your name is AD!"
-elif [ "$NAME" == "AJ" ]
-then
-    echo "Your name is AJ!"
-else 
-    echo "Your name is not AD or AJ!"
-fi
+# if [ "$NAME" == "AD" ]
+# then 
+#     echo "Your name is AD!"
+# elif [ "$NAME" == "AJ" ]
+# then
+#     echo "Your name is AJ!"
+# else 
+#     echo "Your name is not AD or AJ!"
+# fi
+
+
+# COMPARISON
+
+########
+# val1 -eq val2 Returns true if the values are equal
+# val1 -ne val2 Returns true if the values are not equal
+# val1 -gt val2 Returns true if val1 is greater than val2
+# val1 -ge val2 Returns true if val1 is greater than or equal to val2
+# val1 -lt val2 Returns true if val1 is less than val2
+# val1 -le val2 Returns true if val1 is less than or equal to val2
+########
